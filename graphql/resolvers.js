2a315f9704265b58c8139c5313a3c6a5e6cf8746
HYEON(Mac) <antaehyeon@live.co.kr>
2019-02-28 14:41:11 +0900

    Feat: apply Axios and etc (completed GraphQL Toy Project)

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,11 @@
-import { getMovies } from "./db";
+import { getMovies, getMovie, getSuggestions, getHousePrices } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating)
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
+    prices: (_, { pnu, stdrYear }) => getHousePrices(pnu, stdrYear)
   }
 };
 
